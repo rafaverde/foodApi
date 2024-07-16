619e8ba83d80f0b11035330c7fcb813c67785ce1
rafaverde <rafaverde@msn.com>
2024-07-16 09:38:14 -0300

    Token cookie configured for sign in.

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -26,7 +26,16 @@ class SessionsController {
       expiresIn,
     })
 
-    return response.json({ user, token })
+    response.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+      maxAge: 15 * 60 * 1000,
+    })
+
+    delete user.password
+
+    return response.json({ user })
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -8,9 +8,17 @@ const uploadConfig = require("./configs/upload")
 const cors = require("cors")
 const express = require("express")
 const routes = require("./routes")
+const cookieParser = require("cookie-parser")
 
 const app = express()
-app.use(cors())
+
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+  })
+)
 app.use(express.json())
 
 app.use("/files/avatar", express.static(uploadConfig.AVATAR_UPLOADS_FOLDER))
