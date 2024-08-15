9cc013215afd553ee8e60ba6443429bb990396fd
rafaverde <rafaverde@msn.com>
2024-08-15 14:34:21 -0300

    Added new http frontend address to Cors.

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "https://foodexplorer-rafaverde.netlify.app",
+      "https://foodexplorer.vrd.ppg.br",
     ],
     credentials: true,
   })
